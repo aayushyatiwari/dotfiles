ab8caea0f2fa4b94d1f1984d1d6527f731a793bd
Aayushya Tiwari <kb24.aayush@gmail.com>
2026-03-03 14:13:29 +0530

    install.sh added

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp -r i3/.config/i3 ~/.config/
+cp -r polybar/.config/polybar ~/.config/
+cp -r kitty/.config/kitty ~/.config/
+cp -r nvim/.config/nvim ~/.config/
+cp -r rofi/.config/rofi ~/.config/
+cp bash/.bashrc ~/.bashrc
